48d9b6070fcc5a10f8af643f0effa98ad44f7b76
Cynthia Trujillo <developercynthia@gmail.com>
2020-10-25 13:59:27 -0500

    Separating pages from components for debugging

--- src/components/markdown-styles/Link_.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const Link_ = styled.a`
-    color: #6745b0;
-    text-decoration-line: underline;
-    text-decoration-style: double;
-    `
-
-export default Link_;
